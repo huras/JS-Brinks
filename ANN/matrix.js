afaf7ab8c625fe084c89a7bf5a9915cf9abeead5
Huras Alexandre Ferreira dos Santos Gomes <hurat@gmail.com>
2020-01-27 11:36:24 -0200

    ANN - Passing code to ES6

--- ANN/matrix.js
+++ ANN/matrix.js
@@ -1,53 +1,55 @@
 
+class Matrix {
 
-function Matrix(rows, cols){
-    this.rows = rows;
-    this.cols = cols;
-    this.matrix = [];
+    constructor(rows, cols){
+        this.rows = rows;
+        this.cols = cols;
+        this.matrix = [];
 
-    for (let i = 0; i < this.rows; i++) {
-        this.matrix[i] = [];
-        for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] = 0;
-        }
-    }
-}
-
-Matrix.prototype.randomize = function(max = 10){
-    for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] = Math.floor(Math.random() * 10);
-        }
-    }
-}
-
-Matrix.prototype.add = function(n){
-    if( n instanceof Matrix){
         for (let i = 0; i < this.rows; i++) {
+            this.matrix[i] = [];
             for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] += n.matrix[i][j];
-            }
-        }
-    } else {
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] += n;
+                this.matrix[i][j] = 0;
             }
         }
     }
-}
 
-Matrix.prototype.multiply = function(n){
-    if( n instanceof Matrix){
+    randomize (max = 10){
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] *= n.matrix[i][j];
+                this.matrix[i][j] = Math.floor(Math.random() * 10);
             }
         }
-    } else {
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] *= n;
+    }
+
+    add (n){
+        if( n instanceof Matrix){
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] += n.matrix[i][j];
+                }
+            }
+        } else {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] += n;
+                }
+            }
+        }
+    }
+
+    multiply (n){
+        if( n instanceof Matrix){
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] *= n.matrix[i][j];
+                }
+            }
+        } else {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] *= n;
+                }
             }
         }
     }
--- ANN/nn.js
+++ ANN/nn.js
@@ -1,3 +1,7 @@
-// class NeuralNetwork {
-//     constructor(input_nodes, hidden_nodes, outpu_nodes)
-// }
+class NeuralNetwork {
+    constructor(input_nodes, hidden_nodes, outpu_nodes){
+        this.input_nodes = input_nodes;
+        this.hidden_nodes = hidden_nodes;
+        this.outpu_nodes = outpu_nodes;
+    }
+}
