c4ce57fc4e33e1ddeb996a4a1757dd300587e9b7
Huras Alexandre Ferreira dos Santos Gomes <hurat@gmail.com>
2020-01-27 14:37:18 -0200

    ANN - More matrix math + sketch setup

--- ANN/matrix.js
+++ ANN/matrix.js
@@ -22,7 +22,7 @@ class Matrix {
         }
     }
 
-    add (n){
+    add (n) {
         if( n instanceof Matrix){
             for (let i = 0; i < this.rows; i++) {
                 for (let j = 0; j < this.cols; j++) {
@@ -39,18 +39,46 @@ class Matrix {
     }
 
     multiply (n){
-        if( n instanceof Matrix){
-            for (let i = 0; i < this.rows; i++) {
-                for (let j = 0; j < this.cols; j++) {
-                    this.matrix[i][j] *= n.matrix[i][j];
+        if( n instanceof Matrix){ // Matrix multiplication
+            if(this.cols !== n.rows){
+                console.log('Matrizes incompatíveis');
+                return undefined;
+            }
+
+            let result = new Matrix(this.rows, n.cols);
+            let a = this.matrix;
+            let b = n.matrix;
+
+            for (let i = 0; i < result.rows; i++) {
+                for (let j = 0; j < result.cols; j++) {
+                    let sum = 0;
+                    for (let k = 0; k < this.rows; k++) {
+                        sum += a[i][k] * b[k][j];
+                    }
+
+                    result.matrix[i][j] = sum;
                 }
             }
-        } else {
+
+            return result;
+        } else { //Scalar multiplication
             for (let i = 0; i < this.rows; i++) {
                 for (let j = 0; j < this.cols; j++) {
                     this.matrix[i][j] *= n;
                 }
             }
         }
+    }
+
+    transposed () {
+        let result = new Matrix(this.cols, this.rows);
+
+        for (let i = 0; i < result.rows; i++) {
+            for (let j = 0; j < result.cols; j++) {
+                result.matrix[j][i] = this.matrix[i][j];
+            }
+        }
+
+        return result;
     }
 }
